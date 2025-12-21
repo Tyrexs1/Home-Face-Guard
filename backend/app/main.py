b5e92d7b71cc8e3de065072517a3a50a9cb20ff2
Tyrexs1 <robyariga336@gmail.com>
2025-12-21 20:38:26 +0700

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,7 +27,7 @@ def create_app():
     # ✅ CORS (tanpa path, cukup origin domain)
     CORS(
         app,
-        resources={r"/api/*": {"origins": ["https://homefaceguard.netlify.app"]}},
+        resources={r"/api/*": {"origins": ["https://homeface-guard.netlify.app/"]}},
         supports_credentials=False,  # ubah jadi True hanya kalau pakai cookie/session
     )
 
